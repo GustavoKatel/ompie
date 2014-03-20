fa4cdd1fa5adda98164067d346495b359fd3d2eb
Gustavo Brito <gbritosampaio@gmail.cm>
2014-03-20 19:06:47 -0300

    nprimos refeito

--- src/nprimos.c
+++ src/nprimos.c
@@ -31,26 +31,29 @@ int main( int argc, char *argv[] )
 
     int count_per_rank=0, count_global=0;
 
+    // printf("[%d] count_per_rank: %d\n", myid, count_per_rank);
+
     do {    
 
         n++;
 
         int count_per_rank = dividers(n);
         //
+        MPI_Allreduce(&count_per_rank, &count_global, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
-        MPI_Reduce(&count_per_rank, &count_global, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
         // printf("[%d]: count_per_rank: %d, n: %d\n", myid, count_per_rank, n);
         // printf("[%d]: count_global: %d, n: %d\n", myid, count_global, n);
         if(count_global==0) // it's a prime!
         {
             total++;
         }
-      
 
     } while(total<k);
 
     if(!myid)
+    {
         printf("Primo: %d/%d: %d\n", total, k, n);
+    }
 
     endtime   = MPI_Wtime();
     if(!myid)
@@ -64,26 +67,46 @@ int main( int argc, char *argv[] )
 int dividers(int n)
 {
 
-    int range = n / numprocs;
-    int intComp = 0;
-    if(myid==numprocs-1)
+    int range;
+    int start=2;
+    if(n<numprocs)
     {
-        intComp += n - numprocs*range; // compensate the int error
+        if(myid==0)
+            range = n;
+        else
+            range = 0;
+    } else {
+
+        range = n / numprocs;
+        if(n%2==1)
+            range = (n+1) / numprocs;
+
+        start = range * myid + 1;
+
+        if( myid == 0 && n%2==1 )
+        {
+            if(range*numprocs<n)
+                range++;
+            else if(range*numprocs>n)
+                range--;
+        }
+        
     }
 
-    // prime calculation
     int count = 0;
-    int start =  range * myid + 1;
-
-    if(start==1) start++; // skip the first test
-    if(start+range>=n) range = n - start;  // skip the last test. BUGFIX: Single rank range error.
-
     int i;
-    for(i=start;i<start+range+intComp;i++)
+
+    for(i=start;i<start+range;i++)
     {
-        if( ! (n%i) )
+        if(i==1 || i==n)
+            continue;
+        if( n%i == 0 )
+        {
             count++;
+        }
     }
+
+    // printf("[%d] start: %d, range: %d, count: %d\n", myid, start, range, count);
 
     return count;
 
